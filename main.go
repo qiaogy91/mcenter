fbfc121518b32dc93e80b91336c38a54621e67f6
qiaogy91 <9078759+qiaogy91_admin@user.noreply.gitee.com>
2024-11-04 10:12:20 +0800

    去掉Otlp 进行测试

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	_ "github.com/qiaogy91/ioc/config/otlp"
 	"github.com/qiaogy91/ioc/server"
 	_ "github.com/qiaogy91/mcenter/apps"
 )
--- provider/middleware/filter.go
+++ provider/middleware/filter.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"errors"
-	"fmt"
 	"github.com/emicklei/go-restful/v3"
 	"github.com/qiaogy91/ioc/utils"
 	"github.com/qiaogy91/mcenter/apps/token"
@@ -11,7 +10,6 @@ import (
 // Filter 对每一个Rest 请求，内部调用gRPC 接口去校验
 func (i *Impl) Filter() restful.FilterFunction {
 	return func(r *restful.Request, w *restful.Response, chain *restful.FilterChain) {
-		fmt.Printf("我是认证中间件\n")
 		// 没有匹配到路由
 		sr := r.SelectedRoute()
 		if sr == nil {
